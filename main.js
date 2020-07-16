d254f8419c4ca0a20633a212a1bafabae2a87e27
shilichev <52910393+shilichev@users.noreply.github.com>
2020-07-16 20:44:51 +0300

    4th commit

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 let obj = {};
-const check=(obj)=>{alert(obj[1])};
+
+
+const getContr=(obj)=>{
+  let counContr = obj.length;
+  for (i = 0; i <= counContr; i++){
+    $('div.avatar')
+    .html(`<div id = ${i} class = "login">${(obj[i].login)}</div>`);
+  console.log(obj[i].login)
+}};
+
+
+const showContr = (obj)=> {return console.log (obj)};
+
+
 $.ajax({
-    url: "https://api.github.com/repos/thomasdavis/backbonetutorials/contributors",
-    success: check(obj), 
-  }); 
+  url: "https://api.github.com/repos/thomasdavis/backbonetutorials/contributors",
+  success: getContr, 
+}); 
+
