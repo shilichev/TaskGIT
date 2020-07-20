7ccaaea7fa5fdd69a4e3ad765078abca91a40e59
shilichev <52910393+shilichev@users.noreply.github.com>
2020-07-20 16:07:06 +0300

    8th commit

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const API_URL =
   "https://api.github.com/repos/thomasdavis/backbonetutorials/contributors";
-const showContributors = (contributorsData) => {
-  contributorsData.forEach((contributorsElement) => {
-    $("div.contact").append(
-      `<div class ="avatar">
+const changeContributors = (contributorsElement) => {
+  $("div.contact").append(
+    `<div class ="avatar">
           <div  class = "login"><strong>LOGIN:${contributorsElement.login}</strong>
             <div  class = "contributions">CONTRIBUTIONS:${contributorsElement.contributions}
             </div>
@@ -11,119 +10,52 @@ const showContributors = (contributorsData) => {
           <img src="${contributorsElement.avatar_url}" class="image">
           <div class = "message"><p class = "message">Написать</p></div>    
        </div>`
-    );
+  );
+};
+const showContributors = (contributorsData) => {
+  $("div.avatar").remove();
+  contributorsData.forEach((contributorsElement) => {
+    changeContributors(contributorsElement);
   });
 };
-
 const getContributors = () =>
   $.ajax({
     url: API_URL,
     success: showContributors,
   });
 
-getContributors();
-
-const getContributorsGOLD = () =>
-  $.ajax({
-    url: API_URL,
-    success: showContributorsGOLD,
-  });
-
-const showContributorsGOLD = (contributorsData) => {
+const showGoldContributors = (contributorsData) => {
   $("div.avatar").remove();
-
   contributorsData.forEach((contributorsElement) => {
-    console.log(contributorsElement.contributions);
     if (contributorsElement.contributions > 8) {
-      $("div.contact").append(
-        `<div class ="avatar">
-            <div  class = "login"><strong>LOGIN:${contributorsElement.login}</strong>
-              <div  class = "contributions">CONTRIBUTIONS:${contributorsElement.contributions}
-              </div>
-            </div>
-            <img src="${contributorsElement.avatar_url}" class="image">
-            <div class = "message"><p class = "message">Написать</p></div>    
-         </div>`
-      );
+      changeContributors(contributorsElement);
     }
   });
 };
-
-const getContributorsSILVER = () =>
-  $.ajax({
-    url: API_URL,
-    success: showContributorsSILVER,
-  });
-
-const showContributorsSILVER = (contributorsData) => {
-  $("div.avatar").remove();
-
-  contributorsData.forEach((contributorsElement) => {
-    console.log(contributorsElement.contributions);
-    if (
-      contributorsElement.contributions <= 8 &&
-      contributorsElement.contributions > 2
-    ) {
-      $("div.contact").append(
-        `<div class ="avatar">
-            <div  class = "login"><strong>LOGIN:${contributorsElement.login}</strong>
-              <div  class = "contributions">CONTRIBUTIONS:${contributorsElement.contributions}
-              </div>
-            </div>
-            <img src="${contributorsElement.avatar_url}" class="image">
-            <div class = "message"><p class = "message">Написать</p></div>    
-         </div>`
-      );
-    }
-  });
-};
-
-const getContributorsBRONZE = () =>
-  $.ajax({
-    url: API_URL,
-    success: showContributorsBRONZE,
-  });
-
-const showContributorsBRONZE = (contributorsData) => {
-  $("div.avatar").remove();
-
-  contributorsData.forEach((contributorsElement) => {
-    console.log(contributorsElement.contributions);
-    if (contributorsElement.contributions <= 2) {
-      $("div.contact").append(
-        `<div class ="avatar">
-            <div  class = "login"><strong>LOGIN:${contributorsElement.login}</strong>
-              <div  class = "contributions">CONTRIBUTIONS:${contributorsElement.contributions}
-              </div>
-            </div>
-            <img src="${contributorsElement.avatar_url}" class="image">
-            <div class = "message"><p class = "message">Написать</p></div>    
-         </div>`
-      );
-    }
-  });
-};
-
+console.log(getContributors());
 $("select")
-  .change(function () {
-    let str = "";
-    $("select option:selected").each(function () {
-      str += $(this).text();
-    });
-    console.log(str);
-    switch (str) {
-      case "GOLD":
-        getContributorsGOLD();
-        break;
-      case "SILVER":
-        getContributorsSILVER();
-        break;
-      case "BRONZE":
-        getContributorsBRONZE();
-        break;
-      default:
-        console.log("4");
-        break;
-    }
-  })
-  .change();
+.change(function () {
+ let str = "";
+ $("select option:selected").each(function () {
+   str += $(this).text();
+ });
+ console.log(str);
+ switch (str) {
+   case "ALL":
+     getContributors(showContributors);
+     break;
+  case "GOLD":
+    showGoldContributors();
+    break;
+ case "SILVER":
+    getContributorsSILVER();
+   break;
+ case "BRONZE":
+   getContributorsBRONZE();
+   break;
+ default:
+    console.log("4");
+   break;
+ }
+})
+.change();
